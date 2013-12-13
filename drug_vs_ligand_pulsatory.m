80a5532b8b6e6737597c62d898556dc33884b9a7
Bernhard Steiert <bernhard.steiert@fdm.uni-freiburg.de>
2013-12-12 22:48:13 -0500

    Added analysis of pulsatory behavior EGF doses vs. inhibitor doses data-set Added function to calculate radial distances for site i by calling radial_dist(site_i)

--- drug_vs_ligand_pulsatory.m
+++ drug_vs_ligand_pulsatory.m
@@ -1,4 +1,6 @@
-%% Make complete fPCA for data without drugs and drug / EGF concentration dependend
+%% Make analysis of pulsatory behavior for data without drugs and drug / EGF concentration dependend
+% BEWARE: Signals still not scaled between experiments
+% If shift of pulsatory behavior with rising inhibitor is observed, it should still be OK ...
 
 close all
 clear all
@@ -13,11 +15,13 @@ grabdataPath = [remotepath 'Code + Stage and Outputsignal'];
 addpath(grabdataPath)
 
 log_trafo = 1; % log-transform signal
-time_range = [50 515];
+time_range = [200 515];
 
 % Gates for drug signals
-lower_gate_all = 0.95;
-lower_gate_late = 0.97;
+% lower_gate_all = 0.97;
+% lower_gate_late = 0.98;
+lower_gate_all = 0.93;
+lower_gate_late = 0.95;
 
 if log_trafo
     lower_gate_all = log10(lower_gate_all);
@@ -33,6 +37,7 @@ sites_for_harmonics = [4:10 17:-1:11 24:30 37:-1:31 44:50 57:-1:51 64:69];
 
 times = cell(0);
 signals = cell(0);
+signals_raw = cell(0);
 celltype = [];
 
 for isite = sites_for_harmonics
@@ -44,10 +49,12 @@ for isite = sites_for_harmonics
     end
 
     if log_trafo
-        signals{end+1} = log10(intensity);
+        signals_raw{end+1} = log10(intensity);
     else
-        signals{end+1} = intensity;
+        signals_raw{end+1} = intensity;
     end
+    
+    signals{end+1} = signals_raw{end} - repmat(nanmean(signals_raw{end},2),1,size(signals_raw{end},2));
     
     celltype = [celltype ones(1,size(intensity,2))*isite];
 end
@@ -62,24 +69,27 @@ lig_name = 'EGF';
 
 times_drug = cell(0);
 signals_drug = cell(0);
+signals_drug_raw = cell(0);
 celltype_drug = [];
 
 for isite = sites_drug
     if exist(remotepath,'dir')
-        [times_drug{end+1},intensity] = grabdata(isite,dataPath);
+        [times_drug{end+1},intensity] = grabdata_drug(isite,dataPath);
     else
         load(['./Workspaces/site_' num2str(isite) '_only_' lig_name])
         times_drug{end+1} = timestamp;
     end
 
     if log_trafo
-        signals_drug{end+1} = log10(intensity);
+        signals_drug_raw{end+1} = log10(intensity);
     else
-        signals_drug{end+1} = intensity;
+        signals_drug_raw{end+1} = intensity;
     end
     
-        % Gate the signals at this place instead of at each call of grabdata_drug(isite,dataPath)
-    signals_drug{end} = signals_drug{end}(:,(min(signals_drug{end}) >= lower_gate_all) & (min(signals_drug{end}(30:end,:)) >= lower_gate_late));
+    % Gate the signals at this place instead of at each call of grabdata_drug(isite,dataPath)
+    signals_drug_raw{end} = signals_drug_raw{end}(:,(min(signals_drug_raw{end}) >= lower_gate_all) & (min(signals_drug_raw{end}(30:end,:)) >= lower_gate_late));
+    
+    signals_drug{end+1} = signals_drug_raw{end} - repmat(nanmean(signals_drug_raw{end},2),1,size(signals_drug_raw{end},2));
     
     celltype_drug = [celltype_drug ones(1,size(signals_drug{end},2))*isite];
 end
@@ -204,7 +214,7 @@ end
 %% Generate spline fits to data-sets given in sites_for_harmonics and sites_drug
 % close all
 
-nbasis = 20;
+nbasis = 40;
 
 [tmp range_ind_min] = min(abs(timestamp - time_range(1)));
 [tmp range_ind_max] = min(abs(timestamp - time_range(2)));
@@ -214,7 +224,7 @@ range_ind = range_ind_min:range_ind_max;
 [tmp range_ind_max] = min(abs(timestamp_drug - time_range(2)));
 range_ind_drug = range_ind_min:range_ind_max;
 
-basis = create_bspline_basis([timestamp(range_ind(1)) timestamp(range_ind(end))], nbasis);
+basis = create_bspline_basis([min([timestamp(range_ind(1)) timestamp_drug(range_ind_drug(1))]) max([timestamp(range_ind(end)) timestamp_drug(range_ind_drug(end))])], nbasis);
 smoothed_data_lig = smooth_basis(timestamp(range_ind),c_signal(range_ind,:),basis);
 
 smoothed_data_drug = smooth_basis(timestamp_drug(range_ind_drug),c_signal_drug(range_ind_drug,:),basis);
@@ -233,7 +243,7 @@ plot(timestamp(range_ind),c_signal(range_ind,ind_harm),'o')
 %% Make FPCA with data generated in previous block
 % close all
 
-nharm = 4;
+nharm = 3;
 % c_signal_pcastr = pca_fd(smoothed_data, nharm);
 c_signal_pcastr = pca_fd(smoothed_data, nharm, fdPar(basis, int2Lfd(2), 0), 0); % WITHOUT CENTERING!!
 % c_signal_pcastr = varmx_pca(c_signal_pcastr);
@@ -245,110 +255,148 @@ plot_pca_fd(c_signal_pcastr, 1, 0)
 % c_signal_rotpcastr = varmx_pca(c_signal_pcastr);
 % plot_pca_fd(c_signal_rotpcastr, 1, 0)
 
-%% Figure 2A: fPCA with dose-dependend colored ligand level
+%% Plot: Eigenfunctions
 close all
 
-% Define principal components to be plotted
-pcs = [2 3];
-
-% angle = 0;
-angle = -10; % rotation angle to right [degree]
-
-Rmat = [1                     0                     0; ...
-        0                     cos(2*pi*angle/360)  sin(2*pi*angle/360); ...
-        0                     -sin(2*pi*angle/360) cos(2*pi*angle/360)];
+rowstocols = 0.5;
+nrows = ceil(nharm^rowstocols);
+ncols = ceil(nharm / nrows);
 
 flipharm = ones(1,nharm);
-flipharm(1:4) = [-1 1 -1 1];
+% flipharm(1:8) = [1 -1 1 -1 -1 1 1 1];
 
-unitypes = unique(celltype);
+times_fine = linspace(timestamp(range_ind(1)),timestamp(range_ind(end)),501);
 
-sites_remain = 1:length(sites_for_harmonics);
-uni_lig = unique(site_lig_ind(sites_remain));
-lig_min = min(site_lig_dose(sites_remain));
-lig_max = max(site_lig_dose(sites_remain));
-ncolor = 201;
-colmap = flipud(jet(ncolor));
-color_doses = 10.^linspace(max(log10([lig_min 1])),log10(lig_max),ncolor);
+harm_eval = 2 * repmat(sqrt(c_signal_pcastr.values(1:nharm))'.*flipharm(1:nharm),length(times_fine),1) .* eval_fd(c_signal_pcastr.harmfd,times_fine);
 
-rowstocols = 0.3;
-nrows = ceil((length(uni_lig)+1)^rowstocols);
-ncols = ceil((length(uni_lig)+1) / nrows);
+for iplot = 1:nharm
+    subplot(nrows,ncols,iplot)
+    
+    plot(times_fine,harm_eval(:,iplot))
+    xlabel(['Harmonic ' num2str(iplot)])
+    set(gca,'XLim',time_range)
+    set(gca,'YLim',[min(min(harm_eval)) max(max(harm_eval))])
+    
+    hold on
+    plot(time_range,[0 0],'--')
+end
+
+%% Remove all nharm from signal --> only stochastic oscillations remain
+close all
+
+evaluated_fd = eval_fd(c_signal_pcastr.fdhatfd,timestamp_drug(range_ind_drug));
+c_signal_woNharm = c_signal_drug(range_ind_drug,:)-evaluated_fd(:,(size(c_signal,2)+1):end);
+
+plot_sites = sites_drug;
+
+rowstocols = 0.45;
+nrows = ceil(length(plot_sites)^rowstocols);
+ncols = ceil(length(plot_sites) / nrows);
 
 figure
 
+for ip1 = 1:length(plot_sites)
+    ip = plot_sites(ip1);
+    subplot(nrows,ncols,ip)
+    
+    c_signal_single = c_signal_woNharm(:,celltype_drug == ip);
+    
+    first_n = 100;
+    
+    first_n = min(first_n,size(c_signal_single,2));
+    
+    plot(timestamp_drug(range_ind_drug),c_signal_single(:,1:first_n))
+    prop_ind = (ip == plot_sites);
+    title([lig_name ' ' num2str(site_lig_dose_drug(prop_ind)) site_inh_name_drug{prop_ind} num2str(site_inh_dose_drug(prop_ind))])
+    
+    set(gca,'XLim',time_range)
+    
+    hold on
+    plot(timestamp_drug(range_ind_drug),nanmean(c_signal_single,2),'--k')
+    
+    plot([120 120],[-0.04 0.04],'b--')
+%     set(gca,'YLim',[-0.04 0.04])
+end
+
+%% Generate spline fits to data-sets given in sites_for_harmonics (for remaining variation)
+close all
+
+nbasis = 40;
+
+% ind_harm = ismember(celltype,sites_for_harmonics);
+% ind_fit = ~ind_harm;
+
+smoothed_data_woNharm = smooth_basis(timestamp_drug(range_ind_drug),c_signal_woNharm,basis);
+
+f = figure;
+set(f,'DefaultAxesColorOrder',jet(size(c_signal_drug(1,:),2)))
+hold on
+
+plot(smoothed_data_woNharm)
+plot(timestamp_drug(range_ind_drug),c_signal_woNharm,'o')
+
+%% Plot: Histogramm of distance to origin
+close all
+
+rad_dist_thres = 0.03;
+
+figure
+hold on
+
+rowstocols = 0.45;
+nrows = ceil(length(plot_sites)^rowstocols);
+ncols = ceil(length(plot_sites) / nrows);
+
+radial_dist = sqrt(sum(getcoef(smoothed_data_woNharm).^2,1));
+
 posFig = get(gcf,'Position');
-posFig(4) = posFig(4)/2;
+% posFig(3) = posFig(3)/2;
+% posFig(4) = posFig(4)*2;
 set(gcf,'Position',posFig)
 set(gcf,'PaperPosition', [0 0 posFig(3) posFig(4)]./15);
 
-flipped_scores = repmat(flipharm,size(c_signal_pcastr.harmscr,1),1).*c_signal_pcastr.harmscr;
-
-sps = [];
-
-for ilig = 1:length(uni_lig)
+for ip1 = 1:length(plot_sites)
+    ip = plot_sites(ip1);
+    subplot(nrows,ncols,ip)
     
-    tmp = subplot(nrows,ncols,ilig);
-    sps = [sps tmp];
+    baredges = linspace(0,max(radial_dist)+.01,21);
+    bar(baredges,histc(radial_dist(celltype_drug == ip),baredges));
+    
+    prop_ind = (ip == plot_sites);
+    title([lig_name ' ' num2str(site_lig_dose_drug(prop_ind)) site_inh_name_drug{prop_ind} num2str(site_inh_dose_drug(prop_ind))])
+    
+    set(gca,'XLim',[0 max(radial_dist)+.01])
+    if ip == length(plot_sites)
+        xlabel('radial distance')
+    end
+    if ip == 1
+        ylabel('absolute frequency')
+    end
+    
     hold on
-    tmpind = find(site_lig_ind(sites_remain) == uni_lig(ilig));
-    title(site_lig_name{sites_remain(tmpind(1))})
     
-    if ilig == 1
-        ylabel(['PC ' num2str(pcs(2))])
-    end
-    if ilig == length(uni_lig)
-        xlabel(['PC ' num2str(pcs(1))])
-    end
-    
-    x_scores = (Rmat(pcs(1),:) * flipped_scores(:,1:3)')';
-    y_scores = (Rmat(pcs(2),:) * flipped_scores(:,1:3)')';
-    plot(x_scores,y_scores,'.','Color',[.7 .7 .7]);
-    
-    for isite = tmpind
-        % Colored
-        [tmp color_ind] = min(abs(color_doses-site_lig_dose(sites_remain(isite))));
-        mycolor = colmap(color_ind,:);
-        plot(x_scores(celltype == sites_for_harmonics(sites_remain(isite)),:),y_scores(celltype == sites_for_harmonics(sites_remain(isite)),:),'.','Color',mycolor);
-    end
-    
-    set(gca,'XLim',[min(x_scores) max(x_scores)]*1.1)
-    aspRatioFig = posFig(3)/posFig(4);
-    posSubplot = get(gca,'Position');
-    aspRatioSubplot = posSubplot(3)/posSubplot(4);
-%     set(gca,'YLim',get(gca,'XLim')/(aspRatioFig*aspRatioSubplot)-.03) % PC1 vs PC3
-%     set(gca,'YLim',get(gca,'XLim')/(aspRatioFig*aspRatioSubplot)+.03) % PC1 vs PC2
-    set(gca,'YLim',get(gca,'XLim')/(aspRatioFig*aspRatioSubplot)-.1) % PC2 vs PC3
+%     plot([rad_dist_thres rad_dist_thres],get(gca,'YLim'),'--')
 end
 
-set(sps(1),'YLim',get(gca,'XLim')/(aspRatioFig*aspRatioSubplot)-.1) % PC2 vs PC3
+%% Plot: Histogramm of distance to origin (new - overlayed)
+close all
 
-h = get(gca);
-subplot(nrows,ncols,ilig+1)
-clim = log10([lig_min lig_max]);
-clim(1) = max([clim(1) 0]);
-set(gca,'CLim',clim)
-colormap(flipud(jet(ncolor)))
-colorbar('Location','North','XTick',log10([1 2.5 5 10 20 50 100]),'XTickLabel',[0 2.5 5 10 20 50 100])
-set(gca,'Visible','off')
-
-
-% DRUG CASE
+% groups = {[1 2 4 10], [4 10 17 57 64]};
+% resort = {[2 3 1 4], [1 4 2 3 5]};
 
 figure
+
+radial_dist = sqrt(sum(getcoef(smoothed_data_woNharm).^2,1));
 
 sites_remain = 1:length(sites_drug);
 uni_lig = unique(site_lig_dose_drug);
 uni_drug = unique(site_inh_ind_drug(sites_remain));
 drug_min = min(site_inh_dose_drug(sites_remain));
 drug_max = max(site_inh_dose_drug(sites_remain));
-ncolor = 201;
-colmap = flipud(jet(ncolor));
-color_doses = 10.^linspace(max(log10([drug_min .1])),log10(drug_max),ncolor);
 
 rowstocols = 0.3;
 nrows = length(uni_drug);
-ncols = length(uni_lig)+1;
+ncols = length(uni_lig);
 
 posFig = get(gcf,'Position');
 posFig(4) = posFig(4)/3;
@@ -367,41 +415,58 @@ for idrug = 1:length(uni_drug)
     title([lig_name ' '  num2str(site_lig_dose_drug(tmpind2(1))) site_inh_name_drug{sites_remain(tmpind(1))}])
     
     if idrug*ilig == 1
-        ylabel(['PC ' num2str(pcs(2))])
+%         ylabel('relative frequency')
+%         ylabel('estimated density')
+        ylabel('median(radial distance)')
     end
     if idrug == length(uni_drug) && ilig == 1
-        xlabel(['PC ' num2str(pcs(1))])
+%         xlabel('radial distance')
+        xlabel('log10(inhibitor dose)')
     end
     
-    x_scores = (Rmat(pcs(1),:) * flipped_scores(:,1:3)')';
-    y_scores = (Rmat(pcs(2),:) * flipped_scores(:,1:3)')';
-    plot(x_scores,y_scores,'.','Color',[.7 .7 .7]);
+    hold on
     
-    for isite = intersect(tmpind,tmpind2)
-        % Colored
-        [tmp color_ind] = min(abs(color_doses-site_inh_dose_drug(sites_remain(isite))));
-        mycolor = colmap(color_ind,:);
-        drug_ind = size(c_signal,2) + find(celltype_drug == sites_drug(sites_remain(isite)));
-        plot(x_scores(drug_ind,:),y_scores(drug_ind,:),'.','Color',mycolor);
+    mygroup = intersect(tmpind,tmpind2);
+    
+    legend_names = {};
+    legendstyles = [];
+    color = jet(length(mygroup));
+    
+    mx = [];
+    mf = [];
+    for ip = 1:length(mygroup)
+        
+        mydist = radial_dist(celltype_drug == mygroup(ip));
+        if ~isempty(mydist)
+            
+            prop_ind = mygroup(ip);
+            
+%             baredges = linspace(0,max(radial_dist)+.01,26);
+%             barheight = histc(mydist,baredges)./sum(celltype_drug == mygroup(ip));
+%             legendstyles = [legendstyles plot(baredges,barheight,'Color',color(ip,:))];
+
+%             [f,xi] = ksdensity(mydist);
+%             mf = [mf sum(xi.*f)];
+%             legendstyles = [legendstyles plot(xi,f,'Color',color(ip,:))];
+
+%             legend_names{end+1} = num2str(site_inh_dose_drug(prop_ind));
+            
+            mf = [mf median(mydist)];
+            mx = [mx site_inh_dose_drug(prop_ind)];
+        end
+
     end
     
-    set(gca,'XLim',[min(x_scores) max(x_scores)]*1.1)
-    aspRatioFig = posFig(3)/posFig(4);
-    posSubplot = get(gca,'Position');
-    aspRatioSubplot = posSubplot(3)/posSubplot(4);
-%     set(gca,'YLim',get(gca,'XLim')/(aspRatioFig*aspRatioSubplot)-.03) % PC1 vs PC3
-%     set(gca,'YLim',get(gca,'XLim')/(aspRatioFig*aspRatioSubplot)+.03) % PC1 vs PC2
-    set(gca,'YLim',get(gca,'XLim')/(aspRatioFig*aspRatioSubplot)-.1) % PC2 vs PC3
+    mx = log10(mx);
+    mx(mx==-Inf) = -1;
+    plot(mx,mf,'k-')
+    plot([-.8 -.8],get(gca,'YLim'),'b--')
+    
+%     legend(sps(end),legendstyles,legend_names)
+    
+%     set(gca,'XLim',[0 max(radial_dist)])
+%     set(gca,'YLim',[0 .6])
+%     set(gca,'YLim',[0 400])
+
     end
 end
-
-set(sps(1),'YLim',get(gca,'XLim')/(aspRatioFig*aspRatioSubplot)-.1) % PC2 vs PC3
-
-h = get(gca);
-subplot(nrows,ncols,[((idrug-1)*ncols+ilig+1)./2 (idrug-1)*ncols+ilig+1])
-clim = log10([drug_min drug_max]);
-clim(1) = max([clim(1) 0]);
-set(gca,'CLim',clim)
-colormap(flipud(jet(ncolor)))
-colorbar('YTick',log10(linspace(1,10,7)),'YTickLabel',[0 0.25 0.5 1 2.5 5 10])
-set(gca,'Visible','off')
---
+++ radial_dist.m
@@ -0,0 +1,52 @@
+function radial_dist = radial_dist(isite)
+    
+    load('harmonics_slow.mat') % Contains only c_signal_pcastr from all data-sets
+    
+    remotepath = mypath();
+    
+    fdaMPath = [remotepath 'fda'];
+    addpath(fdaMPath)
+
+    grabdataPath = [remotepath 'Code + Stage and Outputsignal'];
+    addpath(grabdataPath)
+
+    log_trafo = 1; % log-transform signal
+    time_range = [200 510];
+    
+    if exist(remotepath,'dir')
+        [timestamp,intensity] = grabdata(isite);
+    else
+        load(['./Workspaces/site_' num2str(isite)])
+    end
+
+    if log_trafo
+        c_signal = log10(intensity);
+    else
+        c_signal = intensity;
+    end
+    
+    c_signal = c_signal - repmat(nanmean(c_signal,2),1,size(c_signal,2));
+    
+    [tmp range_ind_min] = min(abs(timestamp - time_range(1)));
+    [tmp range_ind_max] = min(abs(timestamp - time_range(2)));
+    range_ind = range_ind_min:range_ind_max;
+    
+    harm_basis = create_fd_basis(c_signal_pcastr.harmfd);
+    
+    smoothed_additional = smooth_basis(timestamp(range_ind),c_signal(range_ind,:),harm_basis);
+    
+    harm_eval = eval_basis(harm_basis,timestamp(range_ind));
+    
+    fitcoef = getcoef(smoothed_additional);
+    data_fpca_repr = fitcoef'*harm_eval';
+    
+    c_signal_woNharm = c_signal(range_ind,:)-data_fpca_repr';
+    
+    % Generate spline fit to data-set given in isite (for remaining variation)
+    nbasis = 40;
+    basis = create_bspline_basis([timestamp(range_ind(1)) timestamp(range_ind(end))], nbasis);
+    smoothed_data_woNharm = smooth_basis(timestamp(range_ind),c_signal_woNharm,basis);
+    
+    radial_dist = sqrt(sum(getcoef(smoothed_data_woNharm).^2,1));
+
+end
