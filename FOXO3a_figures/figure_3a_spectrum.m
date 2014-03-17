ff7ba0fa56e70768079f957dc6244fb680aa813b
Bernhard Steiert <bernhard.steiert@fdm.uni-freiburg.de>
2014-03-17 14:36:37 -0400

    Fig3a current version

--- FOXO3a_figures/figure_3a_spectrum.m
+++ FOXO3a_figures/figure_3a_spectrum.m
@@ -2,7 +2,7 @@
 % http://www.wavemetrics.com/products/igorpro/dataanalysis/signalprocessing/powerspectra.htm:
 % Power spectra can be computed for the entire signal at once (a "periodogram") or periodograms of segments of the time signal can be averaged together to form the "power spectral density".
 
-%% Generate workspace
+% % Generate workspace
 % 
 % close all
 % clear all
@@ -75,30 +75,36 @@
 % 
 % nsignal = 40; % #signals to be used for mean calculation
 % % for ip = 1:length(plot_sites)
-% ind_sort_radial_top1000 = ind_sort_radial(4280:end);
+% thres_sorted = .1;
+% ind_sort_radial_top = ind_sort_radial(end-round(length(ind_sort_radial)*thres_sorted)+1:end);
+% ind_sort_radial_bottom = ind_sort_radial(1:round(length(ind_sort_radial)*thres_sorted));
 % 
 % c_signal_single = {};
 % c_signal_single{1} = c_signal;
-% c_signal_single{2} = c_signal(:,ind_sort_radial_top1000);
+% c_signal_single{2} = c_signal(:,ind_sort_radial_top);
+% c_signal_single{3} = c_signal(:,ind_sort_radial_bottom);
 % 
 % % White noise:
-% c_signal_single{3} = .002*repmat(sin(2*pi*timestamp*60/(80*60)),1,size(c_signal,2))+.001*randn(size(c_signal));
+% c_signal_single{4} = .002*repmat(sin(2*pi*timestamp*60/(80*60)),1,size(c_signal,2))+.001*randn(size(c_signal));
 % % Pink noise:
-% c_signal_single{4} = c_signal;
-% c_signal_single{4}(:) = .01*pinknoise(length(c_signal(:)))';
+% c_signal_single{5} = c_signal;
+% c_signal_single{5}(:) = .01*pinknoise(length(c_signal(:)))';
 % 
-% save('fourier_signals','timestamp','c_signal_single')
-% 
+% save('fourier_signals','timestamp','c_signal_single','thres_sorted')
+
 % return
 
 %% Plot
 load('./Workspaces/fourier_signals')
 
-labels = {'Main dataset', 'Only 1000 highest scores', 'White noise \\w Sinus', 'Pink noise'};
+labels = {'Main dataset', sprintf('Only %i%% highest scores',thres_sorted*100), sprintf('Only %i%% lowest scores',thres_sorted*100), 'White noise \\w Sinus', 'Pink noise'};
 colmap = lines(length(labels));
 colmap(1,:) = [0 0 0];
 
 figure
+
+linewidth = 1;
+
 hold on
 
 time_range = [200 500];
@@ -124,8 +130,22 @@ for iplot = 1:length(c_signal_single)
 
         c_signal_fft = [c_signal_fft 2*abs(Y(1:NFFT/2+1)).^2];
     end
+    
+    mean_fft = mean(c_signal_fft,2);
+    std_fft = std(c_signal_fft,[],2);
+    std_fft = std_fft./sqrt(size(c_signal_fft,2)-1); % Std of mean --> 1/sqrt(N-1)
+    
+    legh = [legh semilogx(f,10*log10(mean_fft),'Color',colmap(iplot,:),'LineWidth',linewidth)];
+    
+    tmpx = [f'; flipud(f')];
+    % 3 sigma == 99.7% confidence 
+    tmpy = [10*log10(mean_fft+3*std_fft./2); flipud(10*log10(mean_fft-3*std_fft./2))];
 
-    legh = [legh semilogx(f,10*log10(mean(c_signal_fft,2)),'Color',colmap(iplot,:))];
+    ltmp = patch(tmpx, tmpy, -2*ones(size(tmpx)), ones(size(tmpx)));
+    set(ltmp, 'FaceColor', colmap(iplot,:)*0.1+0.9, 'EdgeColor', colmap(iplot,:)*0.1+0.9,'LineWidth',linewidth);
+    ltmp2 = patch(tmpx, tmpy, -ones(size(tmpx)), ones(size(tmpx)));
+    set(ltmp2, 'LineStyle', '--', 'FaceColor', 'none', 'EdgeColor', colmap(iplot,:)*0.3+0.7,'LineWidth',linewidth);
+    
     legstr{end+1} = labels{iplot};
 
 end
@@ -136,8 +156,8 @@ set(gca,'XScale','log');
 set(gca,'YLim',[-80 -50])
 
 % plot([1/(80*60) 1/(80*60)],get(gca,'YLim'),'k:') % equals 85min period
-plot([1/(300*60) 1/(300*60)],get(gca,'YLim'),'k:') % equals 300min period
-plot([1/(15*60) 1/(15*60)],get(gca,'YLim'),'k:') % equals 15min period
+plot([1/(300*60) 1/(300*60)],get(gca,'YLim'),'k:','LineWidth',linewidth) % equals 300min period
+plot([1/(15*60) 1/(15*60)],get(gca,'YLim'),'k:','LineWidth',linewidth) % equals 15min period
 
 % set(gca,'XTick',[1/(300*60) 1/(80*60) 1/(15*60)],'XTickLabel',{'1/(300*60)','1/(80*60)','1/(15*60)'})
 title('Power Spectrum')
