d0c4a5d4d9dab5e207585a569c5a023e45b4effa
Bernhard Steiert <bernhard.steiert@fdm.uni-freiburg.de>
2017-08-10 15:53:44 +0200

    Clean up Fig. 1C for submission

--- FOXO3a_figures/figure_2a_correlation.m
+++ FOXO3a_figures/figure_2a_correlation.m
@@ -1,20 +1,20 @@
 % Figure 2a: Linear correlation of pERK/pAKT vs. pS294/pS253
 addpath('./Functions/')
 
-[westerndata,description] = xlsread('./Workspaces/westernBlotData');
-liglabels = {'NS','EGF','IGF','FGF','HRG','HGF','EPR','BTC'};
+[westerndata,description] = xlsread('./Workspaces/westernBlotData_final');
+experiments_investigated = 1:2;
+
+liglabels = {'NS','EGF','IGF','HRG','HGF','EPR','BTC'};
 exind = 1;
 timeind = 2;
 ligind = 3;
-obsind = 4:10;
-myobs = [4 5 8 9];
-normind = 11;
+obsind = 4:7;
+myobs = [4 5 6 7];
+normind = 8;
 uni_ligs = unique(westerndata(:,ligind));
-resort = [1 3 5 6 2 8 7 4];
+resort = [1 3 4 5 2 7 6];
 uni_ligs = uni_ligs(resort);
 liglabels = liglabels(resort);
-
-experiments_investigated = 2:3; % Write 2:3 to ignore experiment 1
 
 westerndata = westerndata(ismember(westerndata(:,exind),experiments_investigated),:);
 data = westerndata;
@@ -29,22 +29,6 @@ rowstocols = 0.5;
 nrows = ceil(length(obsind)^rowstocols);
 ncols = ceil(length(obsind) / nrows);
 
-% Plotting raw data
-% close all
-% colmap = lines(length(experiments_investigated));
-% for ifig = 1:length(uni_ligs)
-%     figure
-%     for iplot = 1:length(obsind)
-%         subplot(nrows,ncols,iplot)
-%         hold on
-%         for iexp = experiments_investigated
-%             myind = data(:,exind) == iexp & data(:,ligind) == uni_ligs(ifig);
-%             plot(data(myind,timeind),data(myind,obsind(iplot)),'o-','Color',colmap(iexp,:))
-%             title([liglabels{ifig} ' - ' description{obsind(iplot)}])
-%         end
-%     end
-% end
-
 % Scaling of experiments
 scaledLigs = data(data(:,exind) == experiments_investigated(1),ligind);
 scaledTime = data(data(:,exind) == experiments_investigated(1),timeind);
@@ -55,39 +39,21 @@ pinit = [ones(length(experiments_investigated)-1,length(myobs)); pinit];
 Cov=inv(jacobian'*jacobian);
 scaledData = optRes(length(experiments_investigated):end,:);
 scaledStd = sqrt(diag(Cov));
-scaledStd = scaledStd((length(experiments_investigated)-1)*length(myobs)+1:end);
-scaledStd = reshape(scaledStd,size(scaledData));
+scaledStd = reshape(scaledStd,size(optRes));
+scaledStd = scaledStd(length(experiments_investigated):end,:);
 
 rowstocols = 0.5;
 nrows = ceil(size(scaledData,2)^rowstocols);
 ncols = ceil(size(scaledData,2) / nrows);
 
 % Plotting time-courses
-colmap = [linspace(0,1,length(uni_ligs)-1)' ones(length(uni_ligs)-1,1) ones(length(uni_ligs)-1,1)*.9];
+colmap = [linspace(0,1,length(uni_ligs))' ones(length(uni_ligs),1) ones(length(uni_ligs),1)*.9];
 colmap = hsv2rgb(colmap(1:end-1,:));
 colmap = [colmap; [0 0 0]];
-% close all
-% figure
-% for iplot = 1:size(scaledData,2)
-%     subplot(nrows,ncols,iplot)
-%     hold on
-%     colcount = 1;
-%     for ilig = uni_ligs(2:end)'
-%         myind = logical([1; scaledLigs(2:end) == ilig]); % Plot common timepoint zero for all ligands
-%         if sum(~isnan(scaledData(myind,iplot))) > 1 % not only NaN
-%             plot(scaledTime(myind),scaledData(myind,iplot),'o-','Color',colmap(colcount,:))
-%             errorbar(scaledTime(myind),scaledData(myind,iplot),scaledStd(myind,iplot)./2,'Color',colmap(colcount,:))
-%             colcount = colcount + 1;
-%         end
-%     end
-%     title(description{myobs(iplot)})
-%     ylabel('log_2 fold change [au]')
-%     xlabel('time [min]')
-%     set(gca,'XLim',[-10 490])
-% end
 
 figure
 legh = [];
+stdwidth = 1;
 for iplot = 1:size(scaledData,2)
     subplot(nrows,ncols,iplot)
     hold on
@@ -96,7 +62,7 @@ for iplot = 1:size(scaledData,2)
         myind = logical([1; scaledLigs(2:end) == ilig]); % Plot common timepoint zero for all ligands
         if sum(~isnan(scaledData(myind,iplot))) > 1 % not only NaN
             tmpx = [scaledTime(myind); flipud(scaledTime(myind))];
-            tmpy = [scaledData(myind,iplot) + scaledStd(myind,iplot)./2; flipud(scaledData(myind,iplot) - scaledStd(myind,iplot)./2)];
+            tmpy = [scaledData(myind,iplot) + scaledStd(myind,iplot)./stdwidth; flipud(scaledData(myind,iplot) - scaledStd(myind,iplot)./stdwidth)];
             
             ltmp = patch(tmpx, tmpy, -2*ones(size(tmpx)), ones(size(tmpx)));
             set(ltmp, 'FaceColor', colmap(colcount,:)*0.1+0.9, 'EdgeColor', colmap(colcount,:)*0.1+0.9);
@@ -105,7 +71,6 @@ for iplot = 1:size(scaledData,2)
                                     
                                     
             legh = [legh plot(scaledTime(myind),scaledData(myind,iplot),'x-','Color',colmap(colcount,:))];
-%             errorbar(scaledTime(myind),scaledData(myind,iplot),scaledStd(myind,iplot)./2,'Color',colmap(colcount,:))
             colcount = colcount + 1;
         end
     end
@@ -135,12 +100,10 @@ for ilig = uni_ligs(2:end)' % Ignore NS case
         foxositesratio = [foxositesratio scaledData(myind,3)-scaledData(myind,4)];
         erkaktratio_std = [erkaktratio_std sqrt(sum(scaledStd(myind,3:4).^2,2))];
         foxositesratio_std = [foxositesratio_std sqrt(sum(scaledStd(myind,1:2).^2,2))];
-%         plot(scaledData(myind,1)-scaledData(myind,2),scaledData(myind,3)-scaledData(myind,4)];
-        legh = [legh errorbar(scaledData(myind,1)-scaledData(myind,2),scaledData(myind,3)-scaledData(myind,4),sqrt(sum(scaledStd(myind,3:4).^2./2,2)),markers{colcount},'MarkerFaceColor',colmap(colcount,:),'MarkerEdgeColor',colmap(colcount,:),'Color',colmap(colcount,:))];
-        h = herrorbar(scaledData(myind,1)-scaledData(myind,2),scaledData(myind,3)-scaledData(myind,4),sqrt(sum(scaledStd(myind,1:2).^2./2,2)));
+        legh = [legh errorbar(scaledData(myind,1)-scaledData(myind,2),scaledData(myind,3)-scaledData(myind,4),sqrt(sum(scaledStd(myind,3:4).^2./stdwidth,2)),markers{colcount},'MarkerFaceColor',colmap(colcount,:),'MarkerEdgeColor',colmap(colcount,:),'Color',colmap(colcount,:))];
+        h = herrorbar(scaledData(myind,1)-scaledData(myind,2),scaledData(myind,3)-scaledData(myind,4),sqrt(sum(scaledStd(myind,1:2).^2./stdwidth,2)));
         set(h,'Color',colmap(colcount,:))
         set(h(2),'LineStyle', 'none')
-        errorbar_tick(legh(end),0)
         colcount = colcount + 1;
     end
 end
@@ -156,3 +119,10 @@ set(gca,'XLim',[-7.5 5],'YLim',[-3 3.5],'XTick',[-6:2:4])
 
 text(1,-1,['\chi^2/N = ' num2str(chi2/(2*length(erkaktratio(:))))])
 
+return
+% Write xls file
+description_scaled = {'Time','LigInd','pERK','pAKT','pS294','pS253','pERK_std','pAKT_std','pS294_std','pS253_std'};
+scaled_final = [scaledTime scaledLigs scaledData full(scaledStd)];
+fprintf('%s,',description_scaled{:})
+fprintf('\n')
+dlmwrite('Workspaces/westernBlot_scaled.csv',scaled_final)
